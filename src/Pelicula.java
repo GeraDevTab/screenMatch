61dd9f4a6dce9fe16229e189ea093dd24ba6e395
Gerardo <everythingissofast@gmail.com>
2024-06-01 16:47:52 -0600

    implementacion de metodos en la clase pelicula

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,11 +3,22 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    double sumaEvaluaciones;
+    int totalDeLasEvaluaciones;
 
     void muestraFichaTecnica(){
         System.out.println("Mi pelicula es: " + nombre);
         System.out.println("Fecha de lanzamiento: "+fechaDeLanzamiento);
         System.out.println("Duracion en minutos: "+duracionEnMinutos);
 
+    }
+
+    void evalua(double nota){
+        sumaEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    double calculaMedia(){
+        return sumaEvaluaciones / totalDeLasEvaluaciones;
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -6,6 +6,12 @@ public class Principal {
         miPelicula.duracionEnMinutos = 120;
 
         miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+
+        System.out.println(miPelicula.sumaEvaluaciones);
+        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        System.out.println(miPelicula.calculaMedia());
 
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.nombre = "MAtrix";
@@ -17,8 +23,6 @@ public class Principal {
         //para imprimir la direccion de memoria de un objeto en java
         System.out.println(miPelicula);
         System.out.println(otraPelicula);
-
-
 
     }
 }
