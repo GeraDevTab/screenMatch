d52ba14c5b7e97bd98c1ef8810e2e6e3299c4f69
Gerardo <everythingissofast@gmail.com>
2024-06-19 21:26:59 -0600

    utlizando un record para representar datos de json

--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -1,23 +1,46 @@
 package com.aluracursos.screenmatch.principal;
 
+import com.aluracursos.screenmatch.modelos.Titulo;
+import com.aluracursos.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Escriba el nombre de la pelicula a buscar...");
+        var busqueda = entrada.nextLine();
+        String direccionAPI = "https://www.omdbapi.com/?apikey=4830f843&t="+busqueda;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?apikey=4830f843&t=fenix"))
+                .uri(URI.create(direccionAPI))
                 .build();
 
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String json = response.body();
+
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+        TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(miTituloOmdb);
+        Titulo miTitulo = new Titulo(miTituloOmdb);
+        System.out.println(miTitulo);
+
 
     }
 }
