257793844025d93631977f31ff4b4336cb59cc36
Gerardo <everythingissofast@gmail.com>
2024-06-21 21:05:36 -0600

    implementacion de try catch a clase PrincipalConBusqueda

--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -19,36 +19,45 @@ public class PrincipalConBusqueda {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Escriba el nombre de la pelicula a buscar...");
         var busqueda = entrada.nextLine();
-        String direccionAPI = "https://www.omdbapi.com/?apikey=4830f843&t="+busqueda;
+        String direccionAPI = "https://www.omdbapi.com/?apikey=4830f843&"+
+                "t="+busqueda.replace(" ","+");
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(direccionAPI))
                 .build();
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        String json = response.body();
+            String json = response.body();
 
-        System.out.println(json);
+            System.out.println(json);
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-        TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(miTituloOmdb);
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
 
-        //utilizacion del modulo de try cacth para capturar los errores o excepciones
-        try{
+            TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(miTituloOmdb);
+
+            //utilizacion del modulo de try cacth para capturar los errores o excepciones
+
             Titulo miTitulo = new Titulo(miTituloOmdb);
             System.out.println(miTitulo);
 
         } catch (NumberFormatException evento){
             System.out.println("Ocurrio un error: ");
             System.out.println("el error es: "+ evento.getMessage());
-
         }
+        catch(IllegalArgumentException evento){
+            System.out.println("Error en la URI, verifique la direccion");
+        }
+        catch (Exception evento){
+            System.out.println("Ocurrio un error inesperado");
+        }
+
         System.out.println("Finalizo la ejecucion del programa");
 
     }
